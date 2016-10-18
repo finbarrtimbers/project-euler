4b04a90cfd8787a7f56c08c0dc2ac02f40760b71
Finbarr Timbers <ft@finbarr.ca>
2016-10-18 01:01:04 -0600

    Comprehensive tests for problem 1

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-euler: main.cpp
+euler: main.cpp 1-10.h
 	g++ main.cpp -Werror -Wpedantic -Wall  -o euler
+
+test: tests.cpp *.h
+	g++ tests.cpp -Werror -Wpedantic -Wall -I/usr/local/include/gtest  /usr/local/lib/libgtest.a /usr/local/lib/libgtest_main.a -o tests
--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 
-int problem_1(int n) {
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
-    return sum;
-}
+#include "1-10.h"
 
 int main() {
     std::cout << "Problem 1  (10): " << problem_1(10) << std::endl;
